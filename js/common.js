9d74f90d8ca4b69e48344037579cf290e9454ef1
eunyeongee <73635460+eunyeongee@users.noreply.github.com>
2021-09-10 14:53:26 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -5,7 +5,41 @@ $(function () {
         arrow: true,
         dots: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 800, //화면 사이즈 800px
+                settings: {
+                    arrow: false,
+                }
+					}
+        ]
+    });
+    
+    $('section .review .r_wrap .photo ul li').slice(0, 4).show();
+    
+    $('.review .r_wrap .photo .more a').click(function(e){
+        e.preventDefault();
+
+        $("section .review .r_wrap .photo ul li:hidden").slice(0, 4).fadeIn(500);
+
+        if ($("section .review .r_wrap .photo ul li:hidden").length == 0) {
+
+            $(this).parent('.more').css("display", "none")
+        };
+    });
+    
+    $('.review .r_wrap .m_look_btn a').click(function(e){
+        
+        e.preventDefault();
+
+        $("section .review .r_wrap .photo ul li:hidden").slice(0, 4).fadeIn(500);
+
+        if ($("section .review .r_wrap .photo ul li:hidden").length == 0) {
+
+            $(this).parent('.m_look_btn').css("display", "none")
+        };
+        
     });
 
 });
--- js/common_header.js
+++ js/common_header.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function () {
 
     //pc버전 sns 마우스 오버 효과
     $('.photo ul li').hover(function () {
@@ -15,74 +15,40 @@ $(document).ready(function(){
 
     });
 
-
-    //tab_menu
-    var menuclick = $('#mo_navi');
-    var menuBox = $('.tab_menu');
-    var menuClose = $('#close_btn');
-    var menuBg = $('.tab_bg');
+    var tabClose = $('.close_btn');
+    var tabBg = $('.tab_bg');
 
 
-    menuclick.click(function (e) {
+
+    $('.mo_navi').click(function (e) {
         e.preventDefault();
-        menuBox.animate({
+        $('.tab_menu').animate({
             left: 0
-        }, 700);
-        menuBg.fadeIn(300);
-
+        }, 350);
+        
+        $('.tab_bg').fadeIn(350);
     });
-
-
-    menuClose.click(function (e) {
+    
+    $('.close_btn').click(function(e){
         e.preventDefault();
-        menuBox.animate({
-            left: '-' + 600 + 'px'
-        }, 700);
-        menuBg.fadeOut(300);
-
+        $('.tab_menu').animate({
+            left: '-'+100+'%'
+        }, 350);
+        
+        $('.tab_bg').fadeOut(350);
     });
-
-    //tab_menu
-
-
-
-
-    //아코디언 메뉴
-    //.tab_menu>.tab_wrap>nav> ul> li>a객체(주매뉴)를 저장하여라.
-    var btn = $('.tab_menu>.tab_wrap>nav> ul> li>a');
-    //주메뉴에 마우스 클릭 이벤트 설정
-    btn.click(function (e) {
+    
+    
+    $('.tab_menu>.tab_wrap nav >ul > li > a').click(function(e){
+        
         e.preventDefault();
-        //만약 주메뉴에 class 속성이 active가 아니라면
-        //$(this): 클릭한 주메뉴
-        //attr('class'):class속성, attribute
-        //!=:비교연산자(같지않음)
-        if ($(this).attr('class') != 'active') {
+        
+        $(this).toggleClass('on');
+        $(this).next('.sub').slideToggle(350);
+        
+    })
+    
+    
 
-            //주메뉴의 형제객체(next())는 들어감
-            btn.next().slideUp();
-
-            //주메뉴의 active클래스 제거
-            btn.removeClass('active');
-
-            //클릭한 주메뉴에만 active클래스 추가
-            $(this).addClass('active');
-
-            //클릭한 주메뉴의 형제객체만 내려옴
-            $(this).next().slideDown();
-        }
-
-        //if릐 조건이 거짓이라면 
-        //주메뉴의 active 클래스가 설정 되어있다면 
-        else {
-
-            //클릭한 주메뉴의 active클래스 제거
-            $(this).removeClass('active');
-
-            //클릭한 주메뉴의 형제객체 들어감
-            $(this).next().slideUp();
-        }
-
-    });
 
 });
