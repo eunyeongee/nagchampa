49d6ae833de3a4da961aecc2d81fb8be8bd18c22
eunyeongee <73635460+eunyeongee@users.noreply.github.com>
2021-09-14 00:15:47 +0900

    Add files via upload

--- js/common_footer.js
+++ js/common_footer.js
@@ -1,3 +1,41 @@
-$(function(){
+$(function () {
+
+    var topBtn = $('#top');
+
+
+    /*top버튼 스크롤 탑*/
+
+    topBtn.click(function (e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+            
+            scrollTop: 0
+        }, 500);
+
+    });
+
+    /*top버튼 스크롤 탑*/
     
+    
+    
+    //스크롤 이동시 top버튼 작동코드
+
+    $(window).scroll(function () {
+
+        if ( $('html, body').scrollTop() == 0) {
+
+            topBtn.fadeOut(500);
+            
+        } else {
+            
+            topBtn.fadeIn(500);
+        }
+    });
+
+    //스크롤 이동시 top버튼 작동코드
+
+
+
+
 });
